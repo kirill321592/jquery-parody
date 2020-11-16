2e2e37cdf430aca30c9be51f219d842e5ec3127a
kirill321592 <danila321592@gmail.com>
2020-11-16 19:48:35 +0300

    tabs-fix

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-window.addEventListener('load', function(){
-
-    $('.check').on('focus', function(){
-        $(this).addClass('focus');
-    }).on('blur', function(){
-        $(this).removeClass('focus');
-    }).addClass('shadow')
-    .prop('disabled', true)
-    .fadeOut(500, function(){
-        this.style.display = 'block';
-        this.style.opacity = 0.5;
+window.addEventListener("load", function() {
+  $(".check")
+    .on("focus", function() {
+      $(this).addClass("focus");
+    })
+    .on("blur", function() {
+      $(this).removeClass("focus");
+    })
+    .addClass("shadow")
+    .prop("disabled", true)
+    .fadeOut(500, function() {
+      this.style.display = "block";
+      this.style.opacity = 0.5;
     });
 
-    /*
-    $('.items .item').html('1-1').attr('data-some', 1);
+  $(".items .item")
+    .on("click", function() {
+      var item = $(this);
 
-    console.log($('.items .item').html());
-    console.log($('.items .item').attr('data-some'));*/
-
-    $('.items .item').on('click', function(){
-        var item = $(this);
-
-        console.log(item.css('color'));
-        item.css('color', '#777');
-    }).css({
-        color: '#fff',
-        background: '#f90',
-        cursor: 'pointer'
-    }).attr('data-some', '1').on('dblclick', function(){
-        $(this).fadeOut(500);
+      console.log(item.css("color"));
+      item.css("color", "#777");
+    })
+    .css({
+      color: "#fff",
+      background: "#f90",
+      cursor: "pointer"
+    })
+    .attr("data-some", "1")
+    .on("dblclick", function() {
+      $(this).fadeOut(500);
     });
 });
